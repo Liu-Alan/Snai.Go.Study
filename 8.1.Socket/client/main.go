7b25a8db7d77fdd53226740307990e938698536a
小林 刘 <xiaolin-liu@qq.com>
2020-03-24 18:24:24 +0800

    socket

---
+++ 8.1.Socket/client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage:%s host:port", os.Args[0])
+		os.Exit(1)
+	}
+
+	service := os.Args[1]
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+
+	result, err := ioutil.ReadAll(conn)
+	checkError(err)
+
+	fmt.Println(string(result))
+	os.Exit(0)
+}
---
+++ 8.1.Socket/service/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	daytime := time.Now().String()
+	conn.Write([]byte(daytime))
+}
+
+func main() {
+	service := ":7070"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Fprintf("Fatal error: %s", err.Error())
+			continue
+		}
+
+		go handleClient(conn)
+	}
+}

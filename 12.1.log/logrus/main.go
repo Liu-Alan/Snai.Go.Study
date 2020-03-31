73351f54eec824e60eb90e0c41710fd13b77c01b
小林 刘 <xiaolin-liu@qq.com>
2020-03-31 18:06:04 +0800

    log

---
+++ 12.1.log/logrus/main.go
@@ -0,0 +1,90 @@
+package main
+
+/*
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Info("A walrus appears")
+}
+*/
+/*
+import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func init() {
+	// 日志格式化为JSON而不是默认的ASCII
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// 输出stdout而不是默认的stderr，也可以是一个文件
+	log.SetOutput(os.Stdout)
+
+	// 设置记录日志级别
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("The group's number increased tremendously!")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 100,
+	}).Fatal("The ice breaks!")
+
+	// 通过日志语句重用字段
+	// logrus.Entry返回自WithFields()
+	contextLogger := log.WithFields(log.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
+	})
+
+	contextLogger.Info("I'll be logged with common and other field")
+	contextLogger.Info("Me too")
+}
+*/
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.New()
+
+func init() {
+	// 日志格式化为JSON而不是默认的ASCII
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	// 输出stdout而不是默认的stderr，也可以是一个文件
+	log.SetOutput(os.Stdout)
+
+	// 设置记录日志级别
+	log.SetLevel(logrus.InfoLevel)
+
+	//设置输出到文件
+	file, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.Out = file
+	} else {
+		log.Info("Failed to log to file")
+	}
+}
+
+func main() {
+	log.WithFields(logrus.Fields{
+		"filename": "123.txt",
+	}).Info("打开文件失败")
+}

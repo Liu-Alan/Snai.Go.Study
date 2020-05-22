3e153b03079e64b25e1701e18cef63ef288697f8
Liu-Alan <xiaolin-liu@qq.com>
2020-05-22 12:07:17 +0800

    cookie

--- 6.1.cookie/main.go
+++ 6.1.cookie/main.go
@@ -6,6 +6,10 @@ import (
 	"net/http"
 )
 
+// MaxAge=0或未设置 表示未设置Max-Age属性，客户端会话期有效，关闭客户端删除
+// MaxAge<0 表示立刻删除该cookie，等价于"Max-Age: 0"
+// MaxAge>0 表示存在Max-Age属性，单位是秒
+
 func setcookie(w http.ResponseWriter, r *http.Request) {
 	cookie := http.Cookie{Name: "username", Value: "snail"}
 	cookie.HttpOnly = true
